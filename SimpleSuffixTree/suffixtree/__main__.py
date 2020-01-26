23b59a2460867036d2590542e7edf6d72ac03d26
Sin Shimozono <una.veritas@me.com>
2020-01-27 01:03:39 +0900

    suffixtree

--- SimpleSuffixTree/suffixtree/__main__.py
+++ SimpleSuffixTree/suffixtree/__main__.py
@@ -1,23 +1,58 @@
 
 import sys
 
-def suffixtree_root():
-    return { '':[''] }
+class SuffixTree:
+    labelstr = ''
+    children = dict()
     
-def suffixtree_find(node, astr):
-    while (astr[0] in node):
-        node = node[astr[0]]
-        node[0].
-        astr = astr[1:]
-    return (node, astr)
+    def __init__(self, astr):
+        self.labelstr = astr
+        
+    def __str__(self):
+        ostr = self.label() + '('
+        for ea in self.children:
+            ostr += ea
+            ostr += ', '
+        ostr += ')'
+        return ostr
     
-def suffixtree_add(node, astr):
-    (node, astr) = suffixtree_find(node, astr)
-    node[astr[0]] = [astr]
-
+    def label(self):
+        return self.labelstr
+        
+    def append(self, astr):
+        return self.find(self, astr)
+        
+    def find(self, node, astr):
+        if len(astr) == 0:
+    #         if not '' in node:
+    #             node[''] = ''
+    #         return node[''] 
+            return
+        if astr[0] in node.children:
+            node = astr[0]
+            lstr = node.label()
+            length = 0
+            for a, l in zip(astr, lstr):
+                if a != l :
+                    break
+                length += 1
+            if length == len(lstr):
+                return node
+            else:
+                newnode = dict()
+                newnode[lstr[length]] = [lstr[length:], node]
+                node[lstr[0]] = [lstr[0:length], newnode]
+                anothernewnode = dict()
+                newnode[astr[length]] = [astr[length:], anothernewnode]
+                self.find(newnode, astr[length:])
+            return
+        else:
+            node.children[astr[0]] = SuffixTree(astr)
+            return
+        
 print(sys.argv)
 
-suftree = suffixtree_root()
+stree = SuffixTree('')
 
 with open(sys.argv[1]) as textfile:
     lnum = 0
@@ -27,6 +62,10 @@ with open(sys.argv[1]) as textfile:
             for i in range(0,len(tline) - 1):
                 s = tline[i:]
                 print(s, (lnum, i) )
-                suffixtree_add(suftree,s)
-                print(suftree)
+                stree.append(s)
+                print(stree)
         lnum += 1
+        if lnum > 3:
+            break
+
+print(stree)
