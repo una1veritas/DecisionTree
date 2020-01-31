7c9d33eaf191bf2daa10f3f7e7b33b8e858f9852
Sin Shimozono <una.veritas@me.com>
2020-01-31 12:13:43 +0900

    suffix tree

--- SimpleSuffixTree/suffixtree/__main__.py
+++ SimpleSuffixTree/suffixtree/__main__.py
@@ -1,71 +1,77 @@
-
 import sys
 
 class SuffixTree:
-    labelstr = ''
-    children = dict()
+    label = ''
+    children = None
     
-    def __init__(self, astr):
-        self.labelstr = astr
-        
+    def __init__(self, labelstr):
+        self.label = labelstr
+        self.children = dict()
+    
     def __str__(self):
-        ostr = self.label() + '('
-        for ea in self.children:
-            ostr += ea
-            ostr += ', '
+        if self.is_root() :
+            ostr = 'SuffixTree'
+        else:
+            ostr = self.label
+        if not self.children :
+            ostr += '()'
+            return ostr
+        ostr += '('
+        for key, val in sorted(self.children.items()):
+            ostr += val.__str__() + ', '
+#         path = [self]
+#         while len(path):
+#             for a in sorted(path[-1].children.keys()):
+#                 ostr += path[-1].children[a].__str__() + ', '
+#             path.pop(-1)
         ostr += ')'
         return ostr
+    def is_root(self):
+        return self.label == ''
     
-    def label(self):
-        return self.labelstr
-        
-    def append(self, astr):
-        return self.find(self, astr)
-        
-    def find(self, node, astr):
-        if len(astr) == 0:
-    #         if not '' in node:
-    #             node[''] = ''
-    #         return node[''] 
-            return
-        if astr[0] in node.children:
-            node = astr[0]
-            lstr = node.label()
-            length = 0
-            for a, l in zip(astr, lstr):
-                if a != l :
-                    break
-                length += 1
-            if length == len(lstr):
-                return node
-            else:
-                newnode = dict()
-                newnode[lstr[length]] = [lstr[length:], node]
-                node[lstr[0]] = [lstr[0:length], newnode]
-                anothernewnode = dict()
-                newnode[astr[length]] = [astr[length:], anothernewnode]
-                self.find(newnode, astr[length:])
-            return
+    def append(self, sstr):
+        if len(sstr) == 0 :
+            return self
+        if not sstr[0] in self.children:
+            self.children[sstr[0]] = SuffixTree(sstr)
+            return self.children[sstr[0]]
         else:
-            node.children[astr[0]] = SuffixTree(astr)
-            return
-        
-print(sys.argv)
-
+            achild = self.children[sstr[0]]
+            matchlen = 0
+            for a, c in zip(achild.label, sstr):
+                if a != c :
+                    break
+                matchlen += 1
+            if matchlen == len(achild.label):
+                if self.label == sstr:
+                    return achild
+                else:
+                    return achild.append(sstr[matchlen:])
+            if matchlen < len(achild.label):
+                #print(matchlen, achild.label, sstr)
+                internode = SuffixTree(achild.label[:matchlen])
+                achild.label = achild.label[matchlen:]
+                internode.children[achild.label[0]] = achild
+                self.children[sstr[0]] = internode                
+                if matchlen < len(sstr) :
+                    newnode = SuffixTree(sstr[matchlen:])
+                    internode.children[sstr[matchlen]] = newnode
+                    return newnode
+                return internode
+    
 stree = SuffixTree('')
+print(stree)
 
-with open(sys.argv[1]) as textfile:
-    lnum = 0
-    for tline in textfile:
-        tline = tline.strip()
-        if len(tline) != 0:
-            for i in range(0,len(tline) - 1):
-                s = tline[i:]
-                print(s, (lnum, i) )
-                stree.append(s)
-                print(stree)
-        lnum += 1
-        if lnum > 3:
-            break
+print(sys.argv)
+fname = sys.argv[1]
+with open(fname) as txtdata:
+    for a_line in txtdata:
+        a_line = a_line.strip()
+        if not len(a_line):
+            continue
+        print(a_line)
+        for i in range(0, len(a_line)):
+            #print(a_line[i:])
+            stree.append(a_line[i:])
 
 print(stree)
