8c87d3b57def776e1544de95072f44fd08311af7
Sin Shimozono <una.veritas@icloud.com>
2020-02-06 15:47:23 +0900

    sampletext

--- SimpleDecisionTree/DecisionTree/SimpleDecisionTree.py
+++ SimpleDecisionTree/DecisionTree/SimpleDecisionTree.py
@@ -1,5 +1,7 @@
 #
 #
+import math
+
 class DecisionTree:
     label = None
     children = None
@@ -27,23 +29,34 @@ class DecisionTree:
         ostr += ')'
         return ostr
     
-    def make(self, data, query='SimpleReg'):
+    def make(self, data, propertyIndex, targetIndex, idIndex=None, query='SimpleReg'):
+#         if idIndex != None :
+#             tuples = zip([rec[idIndex] for rec in data], [ rec[propertyIndex] for rec in data], [ rec[targetIndex] for rec in data])
+#         else:
+        
+        if idIndex == None: 
+            tuples = zip(list(range(1,len(data)+1)), [ rec[propertyIndex] for rec in data], [ rec[targetIndex] for rec in data])
+        else:
+            tuples = zip([rec[idIndex] for rec in data], [ rec[propertyIndex] for rec in data], [ rec[targetIndex] for rec in data])
         if query == 'SimpleReg' :
             words = set()
-            for a_line in data:
+            targets = set()
+            dataset = list()
+            for (index, a_line, a_target) in tuples:
                 if len(a_line) == 0 :
                     continue
+                dataset.append( (index, a_line, a_target) )
+                targets.add(a_target)
                 for i in range(0, len(a_line)) :
                     for j in range(i+1, len(a_line)) :
                         words.add(a_line[i:j])
             print(words)
+            print(targets)
             best = 0
             bestword = ''
             for a_word in words:
                 cnt = 0
-                for a_line in data :
-                    if a_word in a_line :
-                        cnt += 1
+                val = self.info_gain(self.classify(a_word, dataset, query))
                 if best < cnt or (best == cnt and len(bestword) < len(a_word) ):
                     best = cnt
                     bestword = a_word
@@ -51,14 +64,42 @@ class DecisionTree:
         else:
             return
         
+    def classify(self, labelobj, dataset, query):
+        if query =='SimpleReg' :
+            res = dict()
+            for (index, a_line, target) in dataset: 
+                if (labelobj in a_line) not in res:
+                    res[(labelobj in a_line)] = dict()
+                if target not in res[(labelobj in a_line)]:
+                    res[(labelobj in a_line)][target] = set()
+                res[(labelobj in a_line)][target].add(index)
+        return res
+    
+    def info_gain(self, results):
+        number_answers = len(results.keys())
+        sum = 0
+        for an_answer in results.keys():
+            subsum = 0
+            for a_decided in results[an_answer].keys():
+                subsum += len(results[an_answer][a_decided]) 
+            number_keys = len(results[an_answer].keys())
+            subinfo = 0
+            print(an_answer, results[an_answer])
+            if number_keys != 0 and subsum != 0 :
+                for a_decided in results[an_answer].keys():
+                    subinfo += math.log(len(results[an_answer][a_decided])/subsum, number_keys)
+            sum += subsum
+        return sum
+            
     def is_empty(self):
         return self.label == None
 
-with open('../会話が不自由な入院患者のやりとり事例/医療データセット.txt') as dbfile:
-    db = dbfile.readlines()
-#print(db)
+db = list()
+with open('./sampletext.csv') as dbfile:
+    for a_record in [ a_line.strip().split() for a_line in dbfile.readlines()]:
+        db.append([ an_item.strip() for an_item in a_record])
 #db_pos = ''.split('\n')
 #db_neg = ''.split('\n')
 dtree = DecisionTree(None)
 print(dtree)
-print(dtree.make(db))
+print(dtree.make(db, 0, 1) )
